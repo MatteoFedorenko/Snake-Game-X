77b60e7c969ab6a4bf4dfbdb62f311c45c5ec515
Matteo Fedorenko <matvii.fedorenko@myenaip.eu>
2024-12-18 09:49:35 +0000

    funzioni e variaboli

---
+++ funzioni.js
@@ -0,0 +1,7 @@
+/*function aggiungere(){
+   const el =  document.getElementById("miaLista");
+   const p = document.getElementsByTagName("p");
+   p.innerHTML = el.length;
+}*/
+const bottoneAggiungi = document.getElementById("bottoneAggiungi");
+const bottoneTogli = document.getElementById("bottoneTogli");
