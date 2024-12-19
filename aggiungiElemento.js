5e1a4cb2cbded96a9e39d33d609c06521b3f2086
Matteo Fedorenko <matvii.fedorenko@myenaip.eu>
2024-12-19 10:59:41 +0000

    aggiungi un elemento + icona cestino

---
+++ aggiungiElemento.js
@@ -0,0 +1,24 @@
+const bottoneAggiungi = document.getElementById("bottoneAggiungi");
+const bottoneTogli = document.getElementById("bottoneTogli");
+const lista = document.getElementById('miaLista');
+
+console.log("lista.children.length");
+
+bottoneAggiungi.addEventListener("click",function(){
+
+    const nuovoElemento = document.createElement("li");
+    const userInput = prompt("inserisci un testo (Tommaso è gay)")
+    const iconaCestino = document.createElement("span")
+
+    nuovoElemento.textContent = userInput
+    iconaCestino.textContent = " 🗑️"
+    iconaCestino.style.cursor = "pointer"
+    nuovoElemento.appendChild(iconaCestino);
+    lista.appendChild(nuovoElemento);
+
+    iconaCestino.addEventListener("click", function(){
+
+        nuovoElemento.remove();
+    })
+})
+
